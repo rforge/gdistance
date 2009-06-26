f6256c3135d55102e027a2ecc654a56cc08ab158
jacobvanetten <jacobvanetten@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-06-26 09:22:30 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gdistance@54 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gdistanalyst/R/writeNexus.R
+++ pkg/gdistanalyst/R/writeNexus.R
@@ -6,7 +6,7 @@ writeNexus <- function(distObject, file = "")
     Diag <- attr(distObject, "Diag")
 	distObject <- as.matrix(distObject)
 	N <- dim(distObject)[1]
-	if(!is.null(names(distObject)))	{taxNames <- names(distObject)}
+	if(!is.null(colnames(distObject)))	{taxNames <- colnames(distObject)}
 	else{taxNames <- paste("taxon_", 1:N, sep="")}
 	cat("#NEXUS\n", file = file)
     cat(paste("[R-package gdistanalyst, ", date(), "]\n\n", sep = ""),
@@ -22,16 +22,15 @@ writeNexus <- function(distObject, file = "")
 
 	if (Diag)
 	{
-		cat("[!\n", file = file, append = TRUE)
-		cat("************************************************\n", file = file, append = TRUE)
-		cat("* Non-interleaved, lower-triangular, diagonals *\n", file = file, append = TRUE)
-		cat("************************************************\n", file = file, append = TRUE)
-		cat("]\n", file = file, append = TRUE)
 		cat("BEGIN DISTANCES;\n", file = file, append = TRUE)
-		cat("  matrix\n", file = file, append = TRUE)		
+		cat("FORMAT\n", file = file, append = TRUE)	
+		cat("  TRIANGLE = LOWER\n", file = file, append = TRUE)		
+		cat("  DIAGONAL\n", file = file, append = TRUE)	
+		cat("  LABELS = LEFT;\n", file = file, append = TRUE)			
+		cat("MATRIX\n", file = file, append = TRUE)		
 		for (i in 1:N)
 		{
-			cat("    ", taxNames[i], distObject[i,1:i], file = file, append = TRUE)
+			cat("  ", taxNames[i], distObject[i,1:i], file = file, append = TRUE)
 			if(i == N) {cat(";", file = file, append = TRUE)}
 			cat("\n", file = file, append = TRUE)
 		}
@@ -40,23 +39,20 @@ writeNexus <- function(distObject, file = "")
 	
 	if (!Diag)
 	{
-		cat("[!\n", file = file, append = TRUE)
-		cat("***************************************************\n", file = file, append = TRUE)
-		cat("* Non-interleaved, lower-triangular, no diagonals *\n", file = file, append = TRUE)
-		cat("***************************************************\n", file = file, append = TRUE)
 		cat("BEGIN DISTANCES;\n", file = file, append = TRUE)
-		cat("    format no diagonals;\n", file = file, append = TRUE)		
-		cat("  matrix\n", file = file, append = TRUE)
-		cat("    ", taxNames[1],"\n", file = file, append = TRUE)
+		cat("FORMAT\n", file = file, append = TRUE)			
+		cat("  TRIANGLE = LOWER\n", file = file, append = TRUE)				
+		cat("  NO DIAGONAL\n", file = file, append = TRUE)		
+		cat("  LABELS = LEFT;\n", file = file, append = TRUE)	
+		cat("MATRIX\n", file = file, append = TRUE)
+		cat("  ", taxNames[1],"\n", file = file, append = TRUE)
 		for (i in 1:(N-1))
 		{
 			cat("    ", taxNames[i+1], distObject[(i+1),1:i], file = file, append = TRUE)
-			if(i == N) {cat(";", file = file, append = TRUE)}
+			if(i == N-1) {cat(";", file = file, append = TRUE)}
 			cat("\n", file = file, append = TRUE)
 		}
 	}
 	
     cat("END;\n", file = file, append = TRUE)
 }
-
-writeNexus(a,"c:/testNexus.txt")

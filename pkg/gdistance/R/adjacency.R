3ca3d13639481b8f1e8877af3c62ff26d33adc98
jacobvanetten <jacobvanetten@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-12-01 01:53:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gdistance@219 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/gdistance/R/adjacency.R
+++ pkg/gdistance/R/adjacency.R
@@ -1,14 +1,14 @@
 # Author: Jacob van Etten jacobvanetten@yahoo.com
-# Date :  September 2011
+# Date :  November 2013
 # Version 1.2
 # Licence GPL v3
 
 adjacencyFromTransition <- function(x)
 {
-	transitionMatr <- as(x,"sparseMatrix")
-	transition.dgT <- as(transitionMatr,"dgTMatrix")
+  tc <- transitionCells(x)
+  x <- transitionMatrix(x)
+	transition.dgT <- as(x,"dgTMatrix")
 	adjacency <- cbind(transition.dgT@i+1,transition.dgT@j+1)
+  adjacency <- cbind(tc[adjacency[,1]], tc[adjacency[,2]])
 	return(adjacency)
-	#cbind(transitionCells(transition)[as.integer(AIndex@i+1)],transitionCells(transition)[as.integer(AIndex@j+1)]) 
 }
-
